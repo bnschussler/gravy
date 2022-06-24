d6fef2fb44eef5db196215fb9ff5f1ab88a0ca4f
bnschussler <59215102+bnschussler@users.noreply.github.com>
2022-06-24 11:19:03 -0500

    see if this fixes pages

--- inputHandling.js
+++ inputHandling.js
@@ -40,10 +40,10 @@ function updateVisibility(show,id){
 
 function updateSettings(id) {	//help from http://processingjs.nihongoresources.com/processing%20on%20the%20web/#interface
 	var pjs = Processing.getInstanceById(id);
-	
+
 	var n = document.getElementById('nslider').value;
 	var t = document.getElementById('tslider').value/20;
-	var g = document.getElementById('gslider').value/10;
+	var g = document.getElementsById('gslider').value/10;
 	var dark=document.getElementById('darkslider').value/100;
 	var size=document.getElementById('sizeslider').value;
 	var startvel=document.getElementById('startvelslider').value;
